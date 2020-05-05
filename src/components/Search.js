c8778a47e7d57f201bc81b749747e51b0dfb5431
Kian <kian.info78@gmail.com>
2020-05-06 00:04:30 +1000

    search

---
+++ src/components/Search.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+
+const Search = (props) => {
+    const [searchValue, setSearchValue] = useState('');
+      
+    const handleSearchInputChanges = (e) =>{
+            setSearchValue(e.target.value)
+    }
+
+    const callSearchFunction = (e) => {
+        e.pereventDefault();
+        props.Search(searchValue)
+    }
+
+    return(
+        <form className="search">
+        <input
+          value={searchValue}
+          onChange={handleSearchInputChanges}
+          type="text"
+        />
+        <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+      </form>
+
+    );
+
+}
+
+export default Search;
