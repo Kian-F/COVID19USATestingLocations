eafb3c77afaf6c6bdb178ae4f2dc3092cac73d19
Kian <kian.info78@gmail.com>
2020-05-08 12:27:19 +1000

    content

--- src/App.js
+++ src/App.js
@@ -1,111 +1,82 @@
-import React, {useState, useEffect} from 'react'
-import Search from './components/Search'
-import styles from './App.modules.css'
-import { Container } from '@material-ui/core';
-import Locations from './components/Locations'
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
+import React, { useState, useEffect } from "react";
+import Search from "./components/Search";
+import styles from "./App.modules.css";
+import { Container } from "@material-ui/core";
+import Locations from "./components/Locations";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Grid from '@material-ui/core/Grid';
 
-import CardContent from '@material-ui/core/CardContent';
+import CardContent from "@material-ui/core/CardContent";
 
-import Typography from '@material-ui/core/Typography';
+import Typography from "@material-ui/core/Typography";
 
+const url = "https://covid-19-testing.github.io/locations/:state/complete.json";
 
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [locations, setLocations] = useState([]);
+  console.log(locations);
 
-const url = 'https://covid-19-testing.github.io/locations/:state/complete.json'
+  // useEffect(() => {
+  //     fetch("{url}/phones/?format=json")
+  //     // .then(response => console.log(response))
 
+  //     .then(jsonResponse => {
+  //         setLocations(jsonResponse.Search);
+  //     })
 
+  // }, [])
 
-const App =() => {
-       
-    
-    const [loading, setLoading] = useState(true);
-    const[locations, setLocations] = useState([]);
-    console.log(locations)
-
-    // useEffect(() => {
-    //     fetch("{url}/phones/?format=json")
-    //     // .then(response => console.log(response))
-        
-    //     .then(jsonResponse => {
-    //         setLocations(jsonResponse.Search);
-    //     })    
-        
-    // }, [])
-
-    const search = searchValue =>{
-        setLoading(true);
-        //setErrorMessage(null);
-        searchValue=searchValue.trim().replace(/ /g, '-').toLowerCase();
-        fetch(`https://covid-19-testing.github.io/locations/${searchValue}/complete.json`)
-        .then(response => response.json())
-       // .then(result => console.log(result))
-        .then(jsonResponse => {
-            
-                setLocations(jsonResponse)
-            
-            
-        },[])
-        
-      
-
-        // var requestOptions = {
-        //     method: 'GET',
-        //     redirect: 'follow'
-        //   };
-          
-        //   fetch("{{url}}/phones/?format=json", requestOptions)
-        //     .then(response => response.text())
-        //     .then(result => console.log(result))
-        //     .catch(error => console.log('error', error));
-        
-    }
-
-   
-    
-
-    return(
-        
-        <div className='hero-image'>
-            <Search className={styles.heroContent} search={search}/>
-                
-           {/* <div>
-               {locations.map(location =>
-               
-               <Locations location={location.name} location={location.id} />
-              
-               
-                 )}
-           </div> */}
-           {locations.map((location, index)=>(
-               
-          <Card className={styles.root}> 
-          <CardContent>
-           
-             <Typography  color="textSecondary" gutterBottom>
-            {location.name}
-           </Typography>
-           <Typography variant="body2" component="p">
-           {location.description}
-           <br />
-           {'"a benevolent smile"'}
-         </Typography>
-                
-                
-               
-                </CardContent>
-        </Card>
-       
-         ))}
-            
-        </div>
-     
-    
+  const search = (searchValue) => {
+    setLoading(true);
+    //setErrorMessage(null);
+    searchValue = searchValue.trim().replace(/ /g, "-").toLowerCase();
+    fetch(
+      `https://covid-19-testing.github.io/locations/${searchValue}/complete.json`
     )
-    
-             
-    }
-    
+      .then((response) => response.json())
+      // .then(result => console.log(result))
+      .then((jsonResponse) => {
+        setLocations(jsonResponse);
+      }, []);
 
+    // var requestOptions = {
+    //     method: 'GET',
+    //     redirect: 'follow'
+    //   };
 
-export  default App;
+    //   fetch("{{url}}/phones/?format=json", requestOptions)
+    //     .then(response => response.text())
+    //     .then(result => console.log(result))
+    //     .catch(error => console.log('error', error));
+  };
+
+  return (
+    <React.Fragment>
+      <div className="hero-image">
+        <Search className={styles.heroContent} search={search} />
+      </div>
+      <Grid item xs={4}>
+      <div className='box'>
+        {locations.map((location, index) => (
+          <Card className={styles.root}>
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                {location.name}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {location.description}
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      </Grid>
+    </React.Fragment>
+  );
+};
+
+export default App;
