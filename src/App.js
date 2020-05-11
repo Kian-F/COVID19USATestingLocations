a49b097f231554876cf24743e6026c612b4e68d5
Kian <kian.info78@gmail.com>
2020-05-11 15:50:34 +1000

    update

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { Container } from "@material-ui/core";
 import Locations from "./components/Locations";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
@@ -27,19 +27,18 @@ const App = () => {
 
   // }, [])
 
-const d = new Date();
-var weekday = new Array(7);
+  const d = new Date();
+  var weekday = new Array(7);
 
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
-weekday[7] = "Sunday";
-var day = weekday[d.getDay()]
-console.log(day)
-
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+  weekday[7] = "Sunday";
+  var day = weekday[d.getDay()];
+  console.log(day);
 
   const search = (searchValue) => {
     setLoading(true);
@@ -71,63 +70,61 @@ console.log(day)
         <Search className={styles.heroContent} search={search} />
       </div>
       <Grid item xs={2}>
-      <div className='box'>
-        {locations.map((location, index) => (
-          <Card className={styles.root}>
-            <CardContent>
-              <Typography color="textSecondary" gutterBottom>
-                {location.name}
-              </Typography>
-        <p className='lastUpdated'>Last Updated: {location.updated}</p>
-              <Typography variant="body2" component="p">
-                {location.description}
-                <br />
-               
-              </Typography>
-              <Grid container spacing={3}>
-        <Grid item xs>
-        {location.regular_schedule.map(day =>
-           
-        <Paper className={styles.paper}>{weekday[day.weekday]}
-    
-        <br/>
-        <br/>
-        Oppen: {day.opens_at}
-        <br/>
-        <br/>
-        Close:{day.closes_at}
-        </Paper>  
-        )}
-    
-     
-        </Grid>
-        {location.physical_address.map(address =>
-            <p>{address.address_1}
-            <br/>
-            {address.city},{' '}
-            
-            {address.state_province}{' '}
-            {address.postal_code}
-            </p>
-            
-            )}
-        </Grid>
-        <Typography>
-        {location.phones.map(phone => 
-        <p>Phone Number: {phone.number}
-        <br/>
-        Type: {phone.type}
-        <br/>
-        Language: {phone.language}
-        </p>
-      
-      )}
-        </Typography>
-    
-            </CardContent>
-          </Card>
-        ))}
-      </div>
+        <div className="box">
+          {locations.map((location, index) => (
+            <Card className={styles.root}>
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  <span style={{ fontSize: 28 }}>{location.name}</span>
+                </Typography>
+                <p className="lastUpdated">Last Updated: {location.updated}</p>
+                <Typography variant="body2" component="p">
+                  {location.description}
+                  <br />
+                </Typography>
+                <Grid container spacing={3}>
+                  <Grid item xs>
+                    {location.regular_schedule.map((day) => (
+                      <Paper className={styles.paper}>
+                        {weekday[day.weekday]}
+                        <br />
+                        <br />
+                        Oppen: {day.opens_at}
+                        <br />
+                        <br />
+                        Close:{day.closes_at}
+                      </Paper>
+                    ))}
+                  </Grid>
+                </Grid>
+                <Grid>
+                  {location.physical_address.map((address) => (
+                    <p>
+                      {" "}
+                      <span style={{ color: "blue" }}>Address:</span> <br />
+                      {address.address_1}
+                      <br />
+                      {address.city}, {address.state_province}{" "}
+                      {address.postal_code}
+                    </p>
+                  ))}
+                </Grid>
+                <Grid>
+                  {location.phones.map((phone) => (
+                    <p>
+                      <span style={{ color: "blue" }}>Phone Number:</span>
+                      {phone.number}
+                      <br />
+                      Type: {phone.type}
+                      <br />
+                      Language: {phone.language}
+                    </p>
+                  ))}
+                </Grid>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       </Grid>
     </React.Fragment>
   );
